62bdb2e85c37c09317419973a6781bc1f48414f0
TomasNemec <tomasnemecml@gmail.com>
2022-01-06 15:07:52 +0100

    added json

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello():
     return 'Hello Heroku!'
 
+@app.route('/json')
+def json():
+    return {
+        "test": "test",
+        "list": [1, 2, 3, 5, 8, 13, 21],
+        "items": [{
+            "name": "item 1",
+            "ID": 1
+        },
+        {
+            "name": "item 2",
+            "ID": 2
+        }]
+    }
+
 if __name__ == '__main__':
     app.run(debug=True)
